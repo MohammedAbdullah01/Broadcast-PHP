d611e1446852ddb0a72b52693e25fe3951e442da
MohamedAbdullah <mm8909840@gmail.com>
2022-11-04 07:32:53 +0200

    Add File doc.php

---
+++ String/doc.php
@@ -0,0 +1,80 @@
+<?php
+
+    /*
+    All The Functions Used  
+        ======================
+        == String Function ==
+        ======================
+
+    1  - lcfirst(String[Required])
+
+    2  - ucfirst(String[Required])
+
+    3  - strtolower(String[Required])
+
+    4  - strtoupper(String[Required])
+
+    5  - ucwords(String[Required] , Delimiters[Optional])
+
+    6  - str_repeat(String[Required] , Count[Required])
+
+    7  - implode(Separator[Optional] , Array[Required]) => Join() Is Alias
+
+    8  - explode(Separator[Required] , String[Required] , Limit[Optional])
+
+    9  - str_shuffle(String[Required])
+
+    10 - strrev(String[Required])
+
+    11 - trim(String[Required] , CharsList[Optional])
+
+    12 - ltrim(String[Required] , CharsList[Optional])
+
+    13 - rtrim(String[Required] , CharsList[Optional])
+
+    14 - chunk_split(String[Required] , Length[Optional] , End[Optional])
+
+    15 - strlen(String[Required])
+
+    16 - str_split(String[Required] , Length[Optional])
+
+    17 - strip_tags(String[Required] , Allowed[Optional])
+
+    18 - nl2br(String[Required] , XHTML[Optional])
+
+    19 - strpos(String[Required] , Value[Required] , Start Position[Opthional]) Case-Sensitive
+
+    20 - strrpos(String[Required] , Value[Required] , Start Position[Opthional]) Case-Sensitive
+
+    21 - stripos(String[Required] , Value[Required] , Start Position[Opthional]) Case-Insensitive
+
+    22 - strripos(String[Required] , Value[Required] , Start Position[Opthional]) Case-Insensitive
+
+    23 - substr_count(String[Required] , Value[Required] , Start Position[Opthional] , Length[Optional])
+
+    24 - parse_str(String[Required] , Array[Required])
+
+    25 - quotemeta(String[Required])
+
+    26 - str_pad(String[Required] , Length[Required] , String[optional] , Pad Flag[Optional])
+
+    27 - strtr(String[Required] , From[Required] , To[Required])
+
+    28 - str_replace(Find[Required] , Replace With[Required] , Data[Required] , Replace Count[Optional]) Its Case Sensitive 
+
+    29 - str_ireplace(Find[Required] , Replace With[Required] , Data[Required] , Replace Count[Optional])
+
+    30 - wordwrap(String[Required] , Width[Optional = 75] , Break_Char[Optional = "\n"] , Cut_Long[Optional = False])
+
+    31 - ord(String[Required])
+
+    32 - chr(Int[Required])
+
+    34 - similar_text(String_One[Required] , String_Two[Required] , Percent[Optional])
+
+    35 - strstr(String[Required] , Search[Required] , Before_Search[Optional] = False) => Alias [strchr] & Case-Sensitive
+
+    36 - stristr(String[Required] , Search[Required] , Before_Search[Optional] = False)  Case-Insensitive
+
+    37 - number_format(Number[Required] , Decimals[Optional] , Decimal_String[Optional] , Separator[Optional])
+    */
