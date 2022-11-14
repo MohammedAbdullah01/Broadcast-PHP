8face83f2decdca575e672f5c6816ece67758dd3
MohamedAbdullah <mm8909840@gmail.com>
2022-11-14 23:47:20 +0200

    Modification File => Array\arrayFunction.php

--- Array/arrayFunctions.php
+++ Array/arrayFunctions.php
@@ -463,4 +463,575 @@ shuffle($shuffle);
 echo "<pre>";
 print_r($shuffle);
 echo "</pre>";
- 
+
+echo "\\*==============================*\\<br>";
+
+
+
+/*
+=======================
+== Array Functions ==
+=======================
+    - array_shift(Array[Required])
+        - Shift An Element Off The Beginning Of Array
+        - This Function Will Reset => "reset()" The Input Array Pointer
+
+    - array_pop(Array[Required] )
+        - Pop The Element Off The End Of Array
+        - This Function Will Reset => "reset()" The Input Array Pointer
+
+    - array_push(Array[Required] , Values[Optional])
+        - Push one or more elements onto the end of array
+        - You Can Use $arr[]
+
+    - array_unshift( Array[Required] , Values[Optional])
+        - Add one or more elements to the beginning of an array
+        - This Function Will Reset => "reset()" The Input Array Pointer
+*/
+
+$chars = ['A', 'B', 'C', 'D'];
+
+// array_shift
+echo "<pre>";
+print_r($chars);
+/*
+Array
+(
+    [0] => A
+    [1] => B
+    [2] => C
+    [3] => D
+)
+ */
+echo "</pre>";
+
+echo array_shift($chars) . "<br>"; // A
+
+echo "<pre>";
+print_r($chars);
+/*
+Array
+(
+    [0] => B
+    [1] => C
+    [2] => D
+)
+ */
+echo "</pre>";
+
+
+// array_pop
+
+
+echo "<pre>";
+print_r($chars);
+/*
+Array
+(
+    [0] => B
+    [1] => C
+    [2] => D
+)
+ */
+echo "</pre>";
+
+echo array_pop($chars) . "<br>"; // D
+
+echo "<pre>";
+print_r($chars);
+/*
+Array
+(
+    [0] => B
+    [1] => C
+)
+ */
+echo "</pre>";
+
+
+// array_push
+// If Add More Element 
+array_push($chars, 'M', 'N', 'K'); // Return (int) => 5
+
+// OR If Add One Element 
+
+$chars[] = "1";
+
+echo "<pre>";
+print_r($chars);
+/*
+Array
+(
+    [0] => B
+    [1] => C
+    [2] => M
+    [3] => N
+    [4] => K
+    [5] => 1
+)
+ */
+echo "</pre>";
+
+
+// array_unshift
+array_unshift($chars, 'One', 'Two', 'Three');
+
+echo "<pre>";
+print_r($chars);
+/*
+Array
+(
+    [0] => One
+    [1] => Two
+    [2] => Three
+    [3] => B
+    [4] => C
+    [5] => M
+    [6] => N
+    [7] => K
+    [8] => 1
+)
+ */
+echo "</pre>";
+
+echo "\\*==============================*\\<br>";
+
+/*
+=======================
+== Array Functions ==
+=======================
+    - array_slice(Array[Required] , Start[Required] , Length[Required] , Preserve_Keys[Optional])
+        - Extract A Slice Of The Array
+            - Start 
+                0 From Start
+                1 From Last Element
+
+            - Length
+                Negative => Stop Slice Untill This Index
+                Not Set  => All Element From Start Position
+
+            - Preserve Keys
+                False => Default => Reset Keys
+                True  => Preserve Keys
+
+        If Array Have Strtin Keys, It Will Always Preserve The Keys
+
+    - array_splice(Array[Required] , Start[Required] , Length[Optional] , Array[Optional])
+        - Remove A Portion Of The Array And Replace It With Something Else
+        
+            Start
+                0 From Start 
+                1 From Last Element
+                
+            Length
+                Negative => Stop Removing Untill This Index
+                Not Set  => Remove All Elements From Start Position
+*/
+
+
+$chars2 = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+
+$chars_with_string_keys  = ['A' => 1, 'B' => 2, 'C' => 3];
+$chars_with_numeric_keys = [10  => 1,  20 => 2,  30 => 3];
+
+/*
+    - Note 
+        - Do Not Be Affected By The Array In => array_slice()
+*/
+echo "<pre>";
+print_r(array_slice($chars2, 2));
+/*
+Array
+(
+    [0] => C
+    [1] => D
+    [2] => E
+    [3] => F
+    [4] => G
+    )
+    */
+
+// Or => Negative
+print_r(array_slice($chars2, -4));
+/*
+Array
+(
+    [0] => D
+    [1] => E
+    [2] => F
+    [3] => G
+)
+*/
+
+// Length
+print_r(array_slice($chars2, 2, 3));
+/*
+Array
+(
+    [0] => C
+    [1] => D
+    [2] => E
+)
+*/
+// Or => Negative
+print_r(array_slice($chars2, 2, -3));
+/*
+Array
+(
+    [0] => C
+    [1] => D
+    )
+    */
+
+// Preserve_Keys
+print_r(array_slice($chars_with_string_keys, 0, 2)); // Maintains If => Preserve Type => String
+/*
+Array
+(
+    [A] => 1
+    [B] => 2
+)
+*/
+print_r(array_slice($chars_with_numeric_keys, 0, 2)); // Does Not Maintain If => Numeric 
+/*
+Array
+(
+    [0] => 1
+    [1] => 2
+)
+*/
+print_r(array_slice($chars_with_numeric_keys, 0, 2, TRUE)); // In the case of keeping the key even if its type is a number add => True 
+/*
+Array
+(
+    [10] => 1
+    [20] => 2
+)
+*/
+
+print_r($chars2); // Array =>  just like it is
+/*
+    Array
+(
+    [0] => A
+    [1] => B
+    [2] => C
+    [3] => D
+    [4] => E
+    [5] => F
+    [6] => G
+)
+*/
+
+
+$nums = [10, 20, 30, 40, 50, 60, 70];
+
+/*
+    - Note 
+        - Affected By Array In => array_splice()
+*/
+
+// array_splice
+
+//Length
+print_r(array_splice($nums, 2)); // Remove from start length => 2 to end  
+/* 
+Array
+(
+    [0] => 30
+    [1] => 40
+    [2] => 50
+    [3] => 60
+    [4] => 70
+)
+*/
+
+print_r($nums); // The Remainder Of Array
+/*
+Array
+(
+    [0] => 10
+    [1] => 20
+)
+*/
+
+print_r(array_splice($nums, 0, 1)); // Remove from start length => 1 Only
+/* 
+Array
+(
+    [0] => 10
+)
+*/
+print_r($nums); // The Remainder Of Array
+/*
+Array
+(
+    [0] => 20
+)
+*/
+
+// Replacement
+print_r(array_splice($nums, 0, 1, [80])); // Remove from start length => 1 only 
+/* 
+Array
+(
+    [0] => 10
+)
+*/
+print_r($nums); // The Remainder Of Array
+/*
+Array
+(
+    [0] => 80
+)
+*/
+echo "</pre>";
+
+echo "\\*==============================*\\<br>";
+
+
+
+/*
+=======================
+== Array Functions ==
+=======================
+    - sort(Array[Required]    , Flag[Optional])
+        - Sort An Indexed Array In Ascending Order
+
+    - rsort(Array[Required]   , Flag[Optional])
+        - Sort An Indexed Array In Descending Order
+
+    - asort(Array[Required]   , Flag[Optional])
+        - Sort An Associative Array In Ascending Order According To The Value
+
+    - arsort(Array[Required]  , Flag[Optional])
+        - Sort An Associative Array In Descending Order According To The Value
+
+    - ksort(Array[Required]   , Flag[Optional])
+        - Sort An Associative Array In Ascending Order According To The Key
+
+    - krsort(Array[Required]  , Flag[Optional])
+        - Sort An Associative Array In Descending Order According To The Key
+
+    - natsort(Array[Required] , Flag[Optional])
+        - Sort An Array By Using A "Natural Order" Algorithm
+*/
+
+
+$names = ['Mohammed', 'Noor', 'Karma', 'Ali', 'Gamal'];
+
+echo "<pre>";
+// Sort
+sort($names);
+print_r($names);
+/*
+Array
+(
+    [0] => Ali
+    [1] => Gamal
+    [2] => Karma
+    [3] => Mohammed
+    [4] => Noor
+)
+*/
+
+// Rsort
+rsort($names);
+print_r($names);
+/*
+Array
+(
+    [0] => Noor
+    [1] => Mohammed
+    [2] => Karma
+    [3] => Gamal
+    [4] => Ali
+)
+*/
+
+$countries4 = ['S' => 'Syria', 'E' => 'Egypt', 'I' => 'Iraq', 'Q' => 'Qatar'];
+
+
+// asort => Sotr Value 
+asort($countries4);
+print_r($countries4);
+/*
+Array
+(
+    [E] => Egypt
+    [I] => Iraq
+    [Q] => Qatar
+    [S] => Syria
+)
+*/
+// arsort => Sotr Value 
+arsort($countries4);
+print_r($countries4);
+/*
+Array
+(
+    [S] => Syria
+    [Q] => Qatar
+    [I] => Iraq
+    [E] => Egypt
+)
+*/
+
+// ksort => Sotr Key 
+ksort($countries4);
+print_r($countries4);
+/*
+Array
+(
+    [E] => Egypt
+    [I] => Iraq
+    [Q] => Qatar
+    [S] => Syria
+)
+*/
+
+// krsort => Sotr Key 
+krsort($countries4);
+print_r($countries4);
+/*
+Array
+(
+    [S] => Syria
+    [Q] => Qatar
+    [I] => Iraq
+    [E] => Egypt
+)
+*/
+
+$files = ['Photo1.png', 'Photo20.png', 'Photo3.png'];
+
+// natsort => Sort Natural Order
+natsort($files);
+print_r($files);
+/*
+Array
+(
+    [0] => Photo1.png
+    [2] => Photo3.png
+    [1] => Photo20.png
+)
+*/
+
+echo "</pre>";
+
+echo "\\*==============================*\\<br>";
+
+
+/*
+=======================
+== Array Functions ==
+=======================
+    - array_map(Callback Function[Required] , Array[Required] , Other Arrays[Optional])
+        - Applies The Callback To The Elements Of The Given Arrays
+
+    - array_filter(Array[Required]  Callback Function[Required] , Flag[Optional])
+        - Filters Elements Of An Array Using A Callback Function
+            - Flag
+                - ARRAY_FILTER_USE_KEY
+                - ARRAY_FILTER_USE_BOTH
+                - 0 Default => Send Value As Arguments
+*/
+
+function sayHello($name)
+{
+    return "Hello : {$name}";
+}
+
+$first_names = ['Ahmed', 'Mohammed', 'Ali', 'Karma', 'Nooh'];
+$last_names  = ['Samy', 'Amr', 'Omar', 'Baraa', 'Ibrahim'];
+
+echo "<pre>";
+print_r(array_map("sayHello", $first_names));
+/*
+Array
+(
+    [0] => Hello : Ahmed
+    [1] => Hello : Mohammed
+    [2] => Hello : Ali
+    [3] => Hello : Karma
+    [4] => Hello : Nooh
+)
+*/
+// OR Anonumous Function
+print_r(array_map(fn ($fname, $lname) => "Hello Mr : {$fname} {$lname}", $first_names, $last_names));
+/*
+Array
+(
+    [0] => Hello Mr : Ahmed Samy
+    [1] => Hello Mr : Mohammed Amr
+    [2] => Hello Mr : Ali Omar
+    [3] => Hello Mr : Karma Baraa
+    [4] => Hello Mr : Nooh Ibrahim
+)
+*/
+echo "</pre>";
+
+
+$array_filter = [
+    1 => 5,
+    9 => 2,
+    6 => 8,
+    3 => 7,
+    8 => 4,
+];
+
+function checkNumber($n)
+{
+    return $n >= 4;
+}
+
+echo "<pre>";
+print_r(array_filter($array_filter, "checkNumber")); // Not Used Flag => Checked Value => Default 0
+/*
+Array
+(
+    [1] => 5
+    [6] => 8
+    [3] => 7
+    [8] => 4
+    )
+*/
+
+// Flag
+print_r(array_filter($array_filter, "checkNumber",  ARRAY_FILTER_USE_KEY)); // Used Flag => Checked Key => ARRAY_FILTER_USE_KEY 
+/*
+Array
+(
+    [9] => 2
+    [6] => 8
+    [8] => 4
+)
+*/
+
+print_r(array_filter($array_filter, fn ($n1, $n2) => $n1 >= 4 && $n2 >= 4,  ARRAY_FILTER_USE_BOTH)); // Used Flag => Checked Key & Value => ARRAY_FILTER_USE_BOTH 
+/*
+Array
+(
+    [6] => 8
+    [8] => 4
+)
+*/
+echo "</pre>";
+
+
+echo "\\*==============================*\\<br>";
+
+
+/*
+=======================
+== Array Functions ==
+=======================
+    - array_reduce( Array[Required] , Callback Function[Required] , Initial_Value[Optional])
+        - Iteratively reduce the array to a single value using a callback function
+        - Reduce The Array To A Single Value
+        - Carry => The Value Of The Previous Iteration || Initial Value
+        - Item => The Value Of The Current Iteration
+*/
+
+
+$nums2 = [10, 20, 50, 4, 9];
