1be68283981a086aa73c201d540519646dd58d9f
MohamedAbdullah <mm8909840@gmail.com>
2022-12-04 08:34:10 +0200

    Modification File => Problems-Solving/Problem-2.php

--- Problems-Solving/Problem-2.php
+++ Problems-Solving/Problem-2.php
@@ -1,5 +1,33 @@
 <?php
 
 /*
-    - Reverce This String Without Any Built In Function In PHP.
+    - Implement Built Function =>  strrev()
+    - Reverse This String Without Any Built In Function In PHP.
+*/
+
+/**
+ * @param string $string
+ * @return string
+ */
+function revstr(string $string): string
+{
+    $stringLength = strlen($string) - 1;
+    $stringrev = "";
+
+    while ($stringLength >= 0) :
+        $stringrev .= $string[$stringLength];
+        $stringLength--;
+    endwhile;
+
+    return $stringrev;
+}
+
+echo revstr("Mohammed"); 
+
+
+/*
+=========
+- Output
+=========
+demmahoM
 */
