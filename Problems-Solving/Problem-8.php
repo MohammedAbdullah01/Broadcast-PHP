2e0d88e487d51090e312093ec5f0341a4d8201f1
MohamedAbdullah <mm8909840@gmail.com>
2022-12-05 11:12:52 +0200

    Modification File => Problems-Solving\Problem-8.php

--- Problems-Solving/Problem-8.php
+++ Problems-Solving/Problem-8.php
@@ -1,76 +1,47 @@
 <?php
 
 /*
-- Write A PHP Program Which Iterates The Integers From 1 To 100 For Multiples Of 
-    Three Print "Three". Instead Of The Number And For The Multiples Of Five Print "Five".
-    For Numbers Which Are Multiples Of Both Three And Five Print "Three And Five".
+- Use Cli
+- Write a PHP Function That Inserts a New Item In An Array In Any Position
  */
 
-for ($i = 1; $i <= 100; $i++):
-    if ($i % 3 == 0 && $i % 5 == 0) 
+$array = [1, 2, 3, 4, 5, 7, 8, 9];
+
+function addElement($array, $element, $index)
+{
+    $new_array = [];
+    foreach($array as  $key => $value )
     {
-        echo "$i => Three And Five \n";
-    } 
-    elseif ($i % 3 == 0) 
-    {
-        echo "$i => Three \n";
-    } 
-    elseif ($i % 5 == 0) 
-    {
-        echo "$i => Five \n";
+        if($index == $key)
+        {
+            $new_array[] = $element;
+        }
+            $new_array[] = $value;
+        
     }
-endfor;
+    return $new_array;
+}
 
+echo "<pre>";
+    print_r(addElement($array , 6 , 5));
+echo "</pre>";
 
-/** 
- * -------
- * OutPut
- * -------
- * 3 => Three
- * 5 => Five
- * 6 => Three
- * 9 => Three
- * 10 => Five
- * 12 => Three
- * 15 => Three And Five
- * 18 => Three
- * 20 => Five
- * 21 => Three
- * 24 => Three
- * 25 => Five
- * 27 => Three
- * 30 => Three And Five
- * 33 => Three
- * 35 => Five
- * 36 => Three
- * 39 => Three
- * 40 => Five
- * 42 => Three
- * 45 => Three And Five
- * 48 => Three
- * 50 => Five
- * 51 => Three
- * 54 => Three
- * 55 => Five
- * 57 => Three
- * 60 => Three And Five
- * 63 => Three
- * 65 => Five
- * 66 => Three
- * 69 => Three
- * 70 => Five
- * 72 => Three
- * 75 => Three And Five
- * 78 => Three
- * 80 => Five
- * 81 => Three
- * 84 => Three
- * 85 => Five
- * 87 => Three
- * 90 => Three And Five
- * 93 => Three
- * 95 => Five
- * 96 => Three
- * 99 => Three
- * 100 => Five
-*/
+/*
+    -------------
+        OutPut
+    -------------
+<pre>
+    Array
+    (
+        [0] => 1
+        [1] => 2
+        [2] => 3
+        [3] => 4
+        [4] => 5
+        [5] => 6
+        [6] => 7
+        [7] => 8
+        [8] => 9
+    )
+</pre>
+ */
