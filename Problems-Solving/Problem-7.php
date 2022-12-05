27e6e8669726426f93555985cc21018a79c5f467
MohamedAbdullah <mm8909840@gmail.com>
2022-12-05 10:39:25 +0200

    Modification File => Problems-Solving\Problem-7.php

--- Problems-Solving/Problem-7.php
+++ Problems-Solving/Problem-7.php
@@ -3,54 +3,72 @@
 /*
 -
  */
-?>
+
+for ($i = 1; $i <= 100; $i++):
+    if ($i % 3 == 0 && $i % 5 == 0) 
+    {
+        echo "$i => Three And Five \n";
+    } 
+    elseif ($i % 3 == 0) 
+    {
+        echo "$i => Three \n";
+    } 
+    elseif ($i % 5 == 0) 
+    {
+        echo "$i => Five \n";
+    }
+endfor;
 
 
-
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-</head>
-
-<body>
-    <h1 class="text-center p-2 my-3">Hello, world!</h1>
-
-    <table class="table table-dark table-hover">
-        <thead>
-            <tr>
-                <?php
-                for ($i = 1; $i <= 10; $i++) : ?>
-                    <th scope="col"><?php echo $i ?></th>
-
-                <?php endfor; ?>
-            </tr>
-
-        </thead>
-        <tbody>
-            
-            <?php
-            for ($a = 1; $a <= 10; $a++) : ?>
-                <tr>
-                    <?php
-
-                    for ($h = 1; $h <= 10; $h++) : ?>
-                        <td class="<?php if ($a == $h) {echo 'bg-success';} ?>">
-                            <?php echo $a * $h ?>
-                        </td>
-
-                    <?php endfor; ?>
-
-                </tr>
-            <?php endfor; ?>
-
-        </tbody>
-    </table>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-</body>
-
-</html>
+/** 
+ * -------
+ * OutPut
+ * -------
+ * 3 => Three
+ * 5 => Five
+ * 6 => Three
+ * 9 => Three
+ * 10 => Five
+ * 12 => Three
+ * 15 => Three And Five
+ * 18 => Three
+ * 20 => Five
+ * 21 => Three
+ * 24 => Three
+ * 25 => Five
+ * 27 => Three
+ * 30 => Three And Five
+ * 33 => Three
+ * 35 => Five
+ * 36 => Three
+ * 39 => Three
+ * 40 => Five
+ * 42 => Three
+ * 45 => Three And Five
+ * 48 => Three
+ * 50 => Five
+ * 51 => Three
+ * 54 => Three
+ * 55 => Five
+ * 57 => Three
+ * 60 => Three And Five
+ * 63 => Three
+ * 65 => Five
+ * 66 => Three
+ * 69 => Three
+ * 70 => Five
+ * 72 => Three
+ * 75 => Three And Five
+ * 78 => Three
+ * 80 => Five
+ * 81 => Three
+ * 84 => Three
+ * 85 => Five
+ * 87 => Three
+ * 90 => Three And Five
+ * 93 => Three
+ * 95 => Five
+ * 96 => Three
+ * 99 => Three
+ * 100 => Five
+*/
