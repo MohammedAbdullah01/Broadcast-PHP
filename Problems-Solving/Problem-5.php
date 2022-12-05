39cac8ada0389df7fd9fd757f5e969f34044d5e5
MohamedAbdullah <mm8909840@gmail.com>
2022-12-05 06:59:33 +0200

    Added File => Problems-Solving\Problem.5.php

---
+++ Problems-Solving/Problem-5.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+    - Implement Built Function => min()
+    - Write a PHP Function To get max Number In Array 
+*/
+
+$arrayNum = [0, 6, 9, 8, 2, 5, 6, 3, 1, 4, 7, 8, 9, 8, -3, -2, 21, 3];
+$arr = [];
+
+/**
+ * @param array $array
+ * @return mixed
+ */
+
+function getMaxNumber(array $array): mixed
+{
+    if (empty($array)) {
+
+        return "This Array Is Empty :(";
+    } else 
+        $maxNumber = $array[0];
+
+    foreach ($array as $value) :
+
+        if ($maxNumber < $value) :
+            $maxNumber = $value;
+        endif;
+
+    endforeach;
+
+    return $maxNumber;
+}
+
+echo getMinimunNumber($arrayNum);
